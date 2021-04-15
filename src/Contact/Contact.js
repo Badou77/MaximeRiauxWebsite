51f102f57198e6b9f05d9f4b994f921a6baed228
Franck Barbedor <franck.barbedor@salesforce.com>
2021-04-15 23:57:58 +0200

    Added contact form (without send email)

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,12 +1,36 @@
 import './Contact.css'
+import React from "react";
+
+class Contact extends React.Component {
+    render() {
+        return (
+            <section id="contact" className="fpage">
+                <h2 className="sectionTitle">Contact</h2>
+
+                <div className="formWrapper">
+                    <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
+                        <div className="form-group">
+                            <label htmlFor="name">Nom complet</label>
+                            <input type="text" className="form-control"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="exampleInputEmail1">Adresse mail</label>
+                            <input type="email" className="form-control" aria-describedby="emailHelp"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="message">Message</label>
+                            <textarea className="form-control" rows="5"/>
+                        </div>
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </section>
+        );
+    }
 
 
-function Contact() {
-    return (
-        <section id="contact" className="fpage">
-            Contact
-        </section>
-    );
+    handleSubmit(event) {
+    }
 }
 
 export default Contact;
