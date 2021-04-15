0f252b05cb53475822d6599752f936aee3956066
Franck Barbedor <franck.barbedor@salesforce.com>
2021-04-15 23:48:14 +0200

    added spotify player

--- src/Realisations/Realisations.js
+++ src/Realisations/Realisations.js
@@ -4,7 +4,12 @@ import './Realisations.css'
 function Realisations() {
     return (
         <section id="realisations" className="fpage">
-            <h2 className="sectionTitle">Réalisations</h2>
+            <h2 className="sectionTitle title-realisations">Réalisations</h2>
+
+            <div className="centeredPlayer spotifyPlayer">
+                <iframe src="https://open.spotify.com/embed?uri=spotify%3Aalbum%3A6anco38F02hxrTtXuU63cW" width="300"
+                        height="380" frameBorder="0" allowTransparency="true" allow="encrypted-media"/>
+            </div>
         </section>
     );
 }
