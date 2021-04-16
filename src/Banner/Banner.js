1e8c0d808d8a0b773986fc5cc67e46718d38a4ac
Franck Barbedor <franck.barbedor@salesforce.com>
2021-04-17 00:34:58 +0200

    Better Banner image

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -3,7 +3,12 @@ import LinkIcons from './BannerComponents/LinkIcons';
 
 function Banner() {
     return (
-        <section id="banner" className="fpage">
+        <section id="banner" className="fpage" style={{
+            backgroundImage: `url(${process.env.PUBLIC_URL + '/bannerMontagnesManWalking.jpg'})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            backgroundPosition: "center"
+        }}>
             <div className="bannerText topSection">
                 <h1 id="bannerTitle">Maxime Riaux</h1>
                 <span>Montage, Mixage, Design sonore, Prise de son</span>
