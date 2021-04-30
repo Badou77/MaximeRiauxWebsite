dd9775e95bf6fc8af0031c79b50f4d60511aa9db
Franck Barbedor <franck.barbedor@salesforce.com>
2021-04-30 20:06:41 +0200

    Test with reflected icons

--- src/Banner/BannerComponents/LinkIcons.js
+++ src/Banner/BannerComponents/LinkIcons.js
@@ -4,51 +4,77 @@ function LinkIcons() {
     return (
         <ul className="linkIcons">
             <li className="linkIcon">
-                <a
-                    href="https://www.linkedin.com/in/maxime-riaux-9116ab125/"
-                    target="_blank" rel="nofollow noopener noreferrer"
-                    className="linkIconLink">
+                <div className={"linkIconContainer"}>
+                    <a
+                        href="https://www.linkedin.com/in/maxime-riaux-9116ab125/"
+                        target="_blank" rel="nofollow noopener noreferrer"
+                        className="linkIconLink">
+                        <img alt="Icône Linkedin"
+                             src={process.env.PUBLIC_URL + '/linkIcons/linkedinWhiteIcon.webp'}
+                             className="linkIconImage"
+                        />
+                    </a>
                     <img alt="Icône Linkedin"
                          src={process.env.PUBLIC_URL + '/linkIcons/linkedinWhiteIcon.webp'}
-                         className="linkIconImage"
-                    />
-                </a>
+                         className="linkIconImage reversedImage"
+                    /></div>
             </li>
 
             <li className="linkIcon">
-                <a
-                    href="https://www.youtube.com/channel/UCFCcAEMumLD45kfb61iF56A"
-                    target="_blank" rel="nofollow noopener noreferrer"
-                    className="linkIconLink">
+                <div className={"linkIconContainer"}>
+
+                    <a
+                        href="https://www.youtube.com/channel/UCFCcAEMumLD45kfb61iF56A"
+                        target="_blank" rel="nofollow noopener noreferrer"
+                        className="linkIconLink">
+                        <img alt="Icône Youtube"
+                             src={process.env.PUBLIC_URL + '/linkIcons/youtubeWhiteIcon.webp'}
+                             className="linkIconImage"
+                        />
+                    </a>
                     <img alt="Icône Youtube"
                          src={process.env.PUBLIC_URL + '/linkIcons/youtubeWhiteIcon.webp'}
-                         className="linkIconImage"
+                         className="linkIconImage reversedImage"
                     />
-                </a>
+                </div>
             </li>
 
             <li className="linkIcon">
-                <a
-                    href="https://shinen-music.bandcamp.com/releases"
-                    target="_blank" rel="nofollow noopener noreferrer"
-                    className="linkIconLink">
+                <div className={"linkIconContainer"}>
+
+                    <a
+                        href="https://shinen-music.bandcamp.com/releases"
+                        target="_blank" rel="nofollow noopener noreferrer"
+                        className="linkIconLink">
+                        <img alt="Icône Bandcamp"
+                             src={process.env.PUBLIC_URL + '/linkIcons/bandcampWhiteIcon.webp'}
+                             className="linkIconImage"
+                        />
+                    </a>
                     <img alt="Icône Bandcamp"
                          src={process.env.PUBLIC_URL + '/linkIcons/bandcampWhiteIcon.webp'}
-                         className="linkIconImage"
+                         className="linkIconImage reversedImage"
                     />
-                </a>
+                </div>
             </li>
 
             <li className="linkIcon">
-                <a
-                    href="https://soundcloud.com/user-838611739"
-                    target="_blank" rel="nofollow noopener noreferrer"
-                    className="linkIconLink">
+                <div className={"linkIconContainer"}>
+
+                    <a
+                        href="https://soundcloud.com/user-838611739"
+                        target="_blank" rel="nofollow noopener noreferrer"
+                        className="linkIconLink">
+                        <img alt="Icône Soundcloud"
+                             src={process.env.PUBLIC_URL + '/linkIcons/soundCloudWhiteIcon.webp'}
+                             className="linkIconImage"
+                        />
+                    </a>
                     <img alt="Icône Soundcloud"
                          src={process.env.PUBLIC_URL + '/linkIcons/soundCloudWhiteIcon.webp'}
-                         className="linkIconImage"
+                         className="linkIconImage reversedImage"
                     />
-                </a>
+                </div>
             </li>
         </ul>
     );
