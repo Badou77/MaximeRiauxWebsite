a141fa8a8be97205ef80423cbc9aa3fdd5cbfd63
Franck Barbedor <franck.barbedor@salesforce.com>
2021-05-02 01:18:13 +0200

    Centered nav bar items

--- src/CustomNavbar.js
+++ src/CustomNavbar.js
@@ -4,10 +4,10 @@ import "./CustomNavbar.css"
 function CustomNavbar() {
     return (
         <Navbar collapseOnSelect expand="lg" variant="dark" sticky="top" className="color-nav">
-            <Navbar.Brand href="#banner">Maxime Riaux</Navbar.Brand>
+            <Navbar.Brand href="#banner" className={"websiteName"}>Maxime Riaux</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
             <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="mr-auto">
+                <Nav className="m-auto">
                     <Nav.Link href="#description">A Propos</Nav.Link>
                     <Nav.Link href="#competences">Compétences</Nav.Link>
                     <Nav.Link href="#sound-records">Réalisations</Nav.Link>
